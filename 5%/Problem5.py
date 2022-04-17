5bc221655a35a31d006c545c48128b826713f327
Aidenwjt <55963813+Aidenwjt@users.noreply.github.com>
2022-04-16 21:39:39 -0600

    Passes

---
+++ 5%/Problem5.py
@@ -0,0 +1,23 @@
+"""
+Problem 5: Smallest multiple
+Description: Find the smallest positive number that is evenly
+divisible by all of the numbers from 1 to 20.
+"""
+
+# TODO: Probably a better starting value and increment value to reduce the runtime
+
+# Start with 40 as this is the next number divisible by 20 after 20
+i = 40
+# While true loop as we will kill the program in the loop
+while(True):
+    # Loop through 1 and 21
+    for j in range(1,21):
+        # Cut if one of the numbers do not evenly divide the current number
+        if(i % j != 0):
+            break
+        # If all values evenly divide the current number then we have found an answer
+        if(j == 20):
+            print('The answer is: {}'.format(i))
+            exit(0)
+    # Increment by the largest divisor
+    i += 20
