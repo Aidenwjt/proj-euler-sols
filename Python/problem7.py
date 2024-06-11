2439db377b49f87b9132e05c3a4cead288760a2a
Aidenwjt <aidenwjt@gmail.com>
2024-06-11 01:36:28 -0600

    Finally finished problem 7.

--- Python/problem7.py
+++ Python/problem7.py
@@ -16,42 +16,29 @@ while(no_primes <= 10001):
 """
 prime_counter = 2
 for i in range(5,N,2):
-    """
-    Translating my C code from
-    https://github.com/Aidenwjt/MATH-327-BYOA/blob/master/src/MRT/mrtest.c
-    """
-    
-    # First need to find values s>0 and odd d>0 such that n-1 = 2^s * d
-    temp = i - 1
     s = 0
-    while(True):
-        if(temp % 2 != 0):
-            d = 1
-            while(2**s * d != i-1):
-                d += 2
-            break
-        temp /= 2
-        s += 1
-
-    # Now start the primality test with witness numbers a = 2 and a = 3
     composite_flag = 0
-    break_flag = 0
+    temp = i - 1
+    while (temp % 2 == 0):
+        s += 1
+        temp = temp/2 
+    d = int((i-1)/(pow(2,s)))
     for a in range(2,4):
         x = pow(a,d,i)
-        for k in range(0,s):
+        y = 0
+        for j in range(0,s):
             y = pow(x,2,i)
-            if((y==1) and (x!=1) and (x!= i-1)):
-                break_flag = 1
+            if (y == 1 and x != 1 and x != i-1):
+                composite_flag = 1
                 break
             x = y
-        if(break_flag == 1):
+        if (composite_flag == 1):
             break
-        if(y != 1):
+        if (y != 1):
             composite_flag = 1
             break
-    if(composite_flag == 0):
+    if (composite_flag != 1):
         prime_counter += 1
-        print("Prime number: {} - Prime counter: {}".format(i,prime_counter))
-    if(prime_counter == 10001):
-        print('The answer is: {}'.format(i))
+    if (prime_counter == 10001):
+        print("{}st: {}".format(prime_counter,i))
         break
