e828a958b956d2b2ce09385bb4beec988d463377
Aidenwjt <aidenwjt@gmail.com>
2024-06-11 02:35:05 -0600

    Added TODO for problem 12.

--- Python/problem12.py
+++ Python/problem12.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
-# TODO: Use prime factorization?
-
-print('The answer is: {}'.format())
+i = 3
+i_prev = 1
+while (True):
+    # TODO: Need to use divisor counting function
+    temp = i
+    i = (i - i_prev) + 1
+    i_prev = temp
